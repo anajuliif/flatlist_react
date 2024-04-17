c9a6e0e8e89d514785b053805ae90124e49d25a4
anajuliif <anafeereira125@gmail.com>
2024-04-17 12:26:05 -0300

    commit

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, FlatList } from "react-native";
+import { styles } from "./styles/Stylesheet";
 
 export default function App() {
+  const data = [
+    { livro: "Matheus", backgroundColor: "#ffffff" },
+    { livro: "Marcos", backgroundColor: "#e6ccef" },
+    { livro: "Lucas", backgroundColor: "#cb9bde" },
+    { livro: "João", backgroundColor: "#af69cd" },
+    { livro: "Eclesiastes", backgroundColor: "#9032bb" },
+    { livro: "Filipenses", backgroundColor: "#7900ac" },
+    { livro: "I Timóteo", backgroundColor: "#6d00a1" },
+    { livro: "II Timóteo", backgroundColor: "#620096" },
+    { livro: "Judas", backgroundColor: "#56008c" },
+    { livro: "Apocalipse", backgroundColor: "#4b0081" },
+  ];
+
+  // Renderiza os Componentes Views(box) com seu respectivo Texto.
+  // Recebe nosso array de objeto, nomeado como data!
+  const renderItem = ({ item }) => (
+    <View style={[styles.box, { backgroundColor: item.backgroundColor }]}>
+      <Text style={styles.txt}>{item.livro}</Text>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}></View>
+      <FlatList
+        horizontal={true}
+        data={data} // Ele está recebendo todos os objetos do nosso array data
+        renderItem={renderItem} // Chama a função para renderizar as Views box
+        keyExtractor={(data) => data.id} // Extrair o id do objeto
+      />
+      <FlatList
+        data={data} // Ele está recebendo todos os objetos do nosso array data
+        renderItem={renderItem} // Chama a função para renderizar as Views box
+        keyExtractor={(data) => data.id} // Extrair o id do objeto
+      />
+      <View style={styles.footer}></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
